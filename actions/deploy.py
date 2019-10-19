2b49744101f0021773b8bd64ca8190087a72c627
Pradipta Das <pradipta.das@nihilent.com>
2019-10-19 22:52:47 +0530

    first commit

--- actions/deploy.py
+++ actions/deploy.py
@@ -1,5 +1,8 @@
 from core import azure_vm_deploy
 
+__all__ = [
+    'deploy_arm_template'
+]
 class deploy_arm_template(azure_vm_deploy.arm_template_provision):
 
     def run(self, client_id,resource_group, subscription_number, tanent_id, serect, region, template_file):
