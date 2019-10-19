c2873446aecde8db2fab0ea4289898c038489f26
Pradipta Das <pradipta.das@nihilent.com>
2019-10-19 22:17:25 +0530

    first commit

---
+++ actions/core/azure_vm_deploy.py
@@ -0,0 +1,61 @@
+from st2common.runners.base_action import Action
+
+from azure.common.credentials import ServicePrincipalCredentials
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.resource.resources.models import DeploymentMode
+
+# from st2client.client import Client
+# from st2client.models import KeyValuePair
+import os,sys,json,random
+
+class arm_template_provision(Action):
+    
+    
+    def __init__(self):
+        super(arm_template_provision, self).__init__()
+
+    def auth(self,client_id,resource_group, subscription_number, tanent_id, serect, region):
+        self.vm_name = 'shield-x_POC_UM'+str(random.randint(1,1001))
+        self.resource_group = resource_group
+        
+        credentials = ServicePrincipalCredentials(
+            client_id = client_id,
+            secret = serect,
+            tenant = tanent_id
+        )
+        resource_group_params = {'location':region}
+        
+        self.resource_group_client = ResourceManagementClient(
+            credentials,
+            subscription_number
+        )
+        
+        self.resource_group_client.resource_groups.create_or_update(resource_group, resource_group_params)
+        
+    def get_template_path(self, existing_template_path):
+        file_path = os.path.dirname(os.path.realpath(__file__ + "../templates/"))
+        return file_path + existing_template_path
+    
+    def deploy_vm(self, existing_template_path):
+        try:
+            template_path = self.get_template_path(existing_template_path)
+
+            with open(template_path) as f:
+                template = json.load(f)
+
+            parameters  = template['parameters']
+            format_parameters = {k: {'value': v} for k, v in parameters.items()}    
+            
+            deployment_properties = {
+                'mode': DeploymentMode.incremental,
+                'template': template,
+                'parameters': format_parameters
+            }
+
+            deployment_async_operation = self.resource_group_client.deployments.create_or_update(self.resource_group,self.vm_name,deployment_properties)
+            result = deployment_async_operation.status()
+            deployment_async_operation.wait()
+            return(True,result)
+
+        except Exception as e:
+            return(False,str(e)) 
---
+++ actions/deploy.py
@@ -0,0 +1,7 @@
+from core import azure_vm_deploy
+
+class deploy_arm_template(azure_vm_deploy.arm_template_provision):
+
+    def run(self, client_id,resource_group, subscription_number, tanent_id, serect, region, template_file):
+        self.auth(client_id,resource_group, subscription_number, tanent_id, serect, region)
+        return self.deploy_vm(template_file)
