cee81dbc430bc41167ef51024305036240b67d6f
Pradipta kumar das <pradipta.das115@gmail.com>
2019-10-22 11:17:35 +0530

    test pack workflow

--- actions/deploy.py
+++
@@ -1,7 +0,0 @@
-from st2common.runners.base_action import Action
-
-class Test(Action):
-
-    def run(self,args):
-        response = "Welcome {}, to stackstorm".format(args)
-        return (True,response)
